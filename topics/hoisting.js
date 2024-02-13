93de19231526278ef2f3d8fce675dc3370ca87a4
priyyadav <yadavpriyanka9628@gmail.com>
2024-02-13 15:52:43 +0530

    varaible declaration

---
+++ topics/hoisting.js
@@ -0,0 +1,25 @@
+let a=10; 
+var b=10;
+const c=10;
+
+
+
+// scoping
+
+// not defined error
+
+// console.log(text)
+
+
+
+// undefined and function
+
+
+console.log(test);
+console.log(data)
+var data=1;
+function test()
+{
+    console.log("hello")
+}
+
