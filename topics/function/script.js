08ebfbae522cd5879a9b1f0f885040866bf246ec
priyyadav <yadavpriyanka9628@gmail.com>
2024-02-13 16:04:03 +0530

    added function

---
+++ topics/function/script.js
@@ -0,0 +1,27 @@
+// function decelartion
+
+
+//  this normal function is hosited inn gec as function and stored as a function
+test()
+function test()
+{
+    console.log("hello js")
+}
+
+// arrow function this is var so it is hosted as variable not a funnction so in gec it will be test1=undefined
+
+// test1()
+var test1=()=>{
+    console.log("arrow funtion")
+}
+
+
+// varaible funtionn
+
+// test2()
+var test1=function()
+{
+    console.log("varaible functio")
+}
+test1()
+test1()
